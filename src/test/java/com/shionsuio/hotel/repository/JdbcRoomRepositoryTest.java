655d8f5456a49b4b1ecc33695fa2eca25ecc8114
shionsuio <shionsuio@gmail.com>
2026-09-08 22:51:26 +0900

    テスト：予約期間が重なる部屋は追加されない

--- src/test/java/com/shionsuio/hotel/repository/JdbcRoomRepositoryTest.java
+++ src/test/java/com/shionsuio/hotel/repository/JdbcRoomRepositoryTest.java
@@ -2,9 +2,11 @@ package com.shionsuio.hotel.repository;
 
 import com.shionsuio.hotel.domain.Room;
 import com.shionsuio.hotel.repository.RoomRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
@@ -20,6 +22,9 @@ public class JdbcRoomRepositoryTest {
     @Autowired
     private RoomRepository roomRepository;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
     void 予約がなければ全部屋を取得できる() {
         List<Room> rooms = roomRepository.findAvailableRoom(
@@ -30,6 +35,35 @@ public class JdbcRoomRepositoryTest {
         assertEquals(
                 List.of(
                         new Room(1L, "101", 10000),
+                        new Room(2L, "102", 12000),
+                        new Room(3L, "201", 15000)
+                ),
+                rooms
+        );
+    }
+
+    @Test
+    @DisplayName("予約期間が重なる部屋は取得されない")
+    void excludeRoomsWithOverlappingReservations() {
+        jdbcTemplate.update("""
+                insert into reservations
+                    (room_id, check_in_date, check_out_date, status)
+                    values (?, ?, ?, ?)
+                """,
+                1L,
+                java.sql.Date.valueOf("2026-10-10"),
+                java.sql.Date.valueOf("2026-10-12"),
+                "CONFIRMED"
+        );
+
+        List<Room> rooms = roomRepository.findAvailableRoom(
+                LocalDate.of(2026, 10,11),
+                LocalDate.of(2026, 10,13)
+
+        );
+
+        assertEquals(
+                List.of(
                         new Room(2L, "102", 12000),
                         new Room(3L, "201", 15000)
                 ),
